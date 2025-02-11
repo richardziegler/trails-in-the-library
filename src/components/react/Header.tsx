6521d985525b91df731da2fc6ffa7210c653bfe6
Richard Ziegler <nablith@gmail.com>
2025-02-11 16:27:10 -0600

    fix up toggle a bit and add header component for initial push to live

---
+++ src/components/react/Header.tsx
@@ -0,0 +1,18 @@
+import { $currentTheme } from "../../stores/themeStore";
+import { useStore } from "@nanostores/react";
+import ThemeToggle from "./ThemeToggle";
+
+export default function Header() {
+    const currentTheme = useStore($currentTheme)
+
+    return (
+        <>
+            <div className={`flex flex-row items-center justify-between ${currentTheme === 'light' ? 'bg-orange-100 text-gray-600' : 'bg-gray-600 text-orange-100'} `}>
+            <div className="ml-6 text-3xl">Welcome to Trails in the Library</div>
+            <div className="flex items-center mt-1.5 mr-5">
+                <ThemeToggle />
+            </div>
+        </div >
+        </>
+    )
+}
